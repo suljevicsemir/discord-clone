c08b4ece771f0907893404dd5ecd7fe4dfbc913d
suljevicsemir <suljevic.semir@gmail.com>
2021-08-10 13:57:29 +0200

    redis and mongo setup

---
+++ node_discord/index.js
@@ -0,0 +1,60 @@
+
+const express = require("express");
+const socketModule = require("socket.io");
+
+const dotenv = require("dotenv");
+dotenv.config({path: "./config/config.env"});
+
+const connectDB = require("./config/mongodb");
+
+const redisClient = require("./config/redis_client");
+
+const SERVER_PORT = process.env.PORT || 4000;
+
+
+
+
+var app = express();
+app.use(express.static("public"));
+
+
+var server = app.listen(SERVER_PORT, () => {
+    console.log("Listening to requests on port: " + SERVER_PORT.toString() + " in " + process.env.NODE_ENV + " mode.");
+    connectDB();
+});
+
+
+
+var io = socketModule(server);
+
+io.on("connection", (socket) => {
+    console.log("Made socket connection:" + socket.id);
+
+    socket.on("login", (data) => {
+
+        //verify jwt
+        //update it's status in redis
+        //store user id for the socket id
+       console.log(data.jwt);
+    });
+
+    socket.on('disconnect', async () => {
+        console.log('Got disconnect!');
+    
+        //get user that is using the socket id
+        const user = redisClient.getSomething(socket.id);
+            
+     });
+     
+
+     socket.on("message", (message) => {
+        
+     });
+
+});
+
+
+
+
+
+
