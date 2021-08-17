de1470e5eb675fd3bfebff869473d126bd953206
suljevicsemir <suljevic.semir@gmail.com>
2021-08-18 01:34:14 +0200

    removing config file from version control

---
+++ node_discord/index.js
@@ -0,0 +1,63 @@
+
+const express = require("express");
+const socketModule = require("socket.io");
+
+const dotenv = require("dotenv");
+dotenv.config({path: "./config/config.env"});
+
+const connectDB = require("./config/mongodb");
+
+const redisClient = require("./config/redis_client");
+
+const SERVER_PORT = process.env.PORT || 4000;
+
+const accountRoutes = require("./routes/discord_accounts");
+
+
+var app = express();
+app.use(express.static("public"));
+app.use(express.json());
+
+var server = app.listen(SERVER_PORT, () => {
+    console.log("Listening to requests on port: " + SERVER_PORT.toString() + " in " + process.env.NODE_ENV + " mode.");
+    connectDB();
+
+
+    app.use("/discord_accounts", accountRoutes);
+});
+
+
+
+var io = socketModule(server);
+
+io.on("connection", (socket) => {
+    console.log("Made socket connection:" + socket.id);
+
+    socket.on("login", (data) => {
+
+        //verify jwt
+        //update it's status in redis
+        //store user id for the socket id
+       console.log(data.jwt);
+    });
+
+    socket.on('disconnect', async () => {
+        console.log('Got disconnect!');
+    
+        //get user that is using the socket id
+        const user = redisClient.getSomething(socket.id);
+            
+     });
+     
+
+     socket.on("message", (message) => {
+        
+     });
+
+});
+
+
+
+
+
+
---
+++ node_discord/routes/discord_accounts.js
@@ -0,0 +1,119 @@
+const router = require("express").Router();
+const mongoose = require("mongoose");
+const redisClient = require("../config/redis_client");
+const DiscordAccount = require("../models/discord_account");
+const jwt = require("jsonwebtoken");
+
+
+
+
+// search accounts with query
+router.get("/", async (req, res) => {
+    try {
+        
+
+       const x = await mongoose.connection.db.collection("discord_accounts").find({}).toArray();
+
+        /*mongoose.connection.db.collection("discord_accounts").find({
+            "firstName" : /$/,
+        }, options).toArray((err, result) => {
+            if(err) throw err;
+            console.log(result);
+            res.send(result);
+        });*/
+        res.json(x);
+
+        
+    }
+    catch(err) {
+        res.send(err);
+    }
+});
+
+// get specific account
+router.get("/:id", async (req, res) => {
+    try {
+        let cachedAccount = await redisClient.getDiscordAccount(req.params.id);
+
+        if( cachedAccount != null) {
+            cachedAccount.fromCache = true;
+            return res.status(200).json(cachedAccount);
+            
+        }
+        
+        cachedAccount = await DiscordAccount.findById(req.params.id);
+        cachedAccount.fromCache = false;
+        const resStatus = cachedAccount == null ? 200 : 404;
+        res.status(resStatus).json(cachedAccount);
+    }
+    catch(err) {
+        res.status(400).json(err);
+    }
+});
+
+// insert new account
+router.post("/", async (req, res) => {
+    try {
+        const discordAccount = DiscordAccount(req.body);
+        const savedAccount = await discordAccount.save();
+        const redisAccount = await redisClient.setDiscordAccount(savedAccount);
+        const token = jwt.sign({"_id" : savedAccount._id.toString()}, process.env.SECRET);
+        res.header("Authorization", token).json(savedAccount);
+    }
+    catch(err) {
+        res.json(err);
+    }
+});
+
+// delete account
+router.delete("/:id", async (req, res) => {
+    try {
+
+    }
+    catch(err) {
+        
+    }
+});
+
+// update account
+router.put("/:id", async (req, res) => {
+    try {
+
+        const token = req.headers.authorization;
+
+        if(!token) {
+            return res.status(403).json({"error" : "User not authenticated!"});
+        }
+        
+        const decodedToken = jwt.verify(token, process.env.SECRET);
+        
+        if( decodedToken._id != req.params.id) {
+            return res.status(403).json({"error" : "JWT not valid!"});
+        }
+        
+        const updatedAccount = await DiscordAccount.updateOne(
+            {
+                _id: req.params.id
+            },
+            {
+                $set: req.body
+            }
+        );
+
+        let account = req.body;
+        account._id = req.params.id;
+        
+        await redisClient.updateDiscordAccount(account);
+    
+        res.json(updatedAccount);
+
+    }
+    catch(err) {
+        res.json(err);
+    }
+});
+
+
+
+module.exports = router;
+
